07a6b868bd1f77d4cf8fbce0cde01e85d7988bb7
andrzej morawski <andrzej.morawski@g3m.pl>
2017-12-31 16:11:27 +0100

    0.2.0

---
+++ http/index.php
@@ -0,0 +1,43 @@
+<!-- kkksfdf  fdsfdsdfs ->>
+
+<?php
+
+	session_start();
+	
+	if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true))
+	{
+		header('Location: gra.php');
+		exit();
+	}
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Rekrutacja - formularz zgłoszeniowy 2</title>
+</head>
+
+<body>
+	
+	I strona formularza<br /><br />
+	
+	<a href="rejestracja.php">Rejestracja - przejdź do wypełniania formularza <a/>
+	<br /><br />
+	
+	<form action="zaloguj.php" method="post">
+	
+		Login: <br /> <input type="text" name="login" /> <br />
+		Hasło: <br /> <input type="password" name="haslo" /> <br /><br />
+		<input type="submit" value="Zaloguj się" />
+	
+	</form>
+	
+<?php
+	if(isset($_SESSION['blad']))	echo $_SESSION['blad'];
+?>
+
+</body>
+</html>
